71256acd5737cb4b43d6ff5bd29cc4849d989229
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-04-07 17:46:38 -0400

    Size and set finished

--- src/main/java/com/github/zipcodewilmington/DashaMap.java
+++ src/main/java/com/github/zipcodewilmington/DashaMap.java
@@ -9,10 +9,24 @@ public class DashaMap implements HashMapX{
 
     Node[] arr = new Node[26];
 
+    private int hashFunctionOne(String input) {
+        if (input.length() > 0) {
+            return input.toLowerCase().charAt(0) - 97;
+        }
+        return -1;
+    }
+
     @Override
     public void set(String key, String value) {
-
-
+        int index = hashFunctionOne(key);
+        Node current = arr[index];
+        if(current == null){
+            current = new Node(key, value);
+        }
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(new Node(key, value));
     }
 
     @Override
@@ -23,21 +37,40 @@ public class DashaMap implements HashMapX{
 
     @Override
     public String get(String key) {
+
         return null;
     }
 
     @Override
     public boolean isEmpty() {
+
         return false;
     }
 
     @Override
     public long size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            Node current = arr[i];
+
+            while(current.next != null){
+                count++;
+                current = current.getNext();
+            }
+        }
+        return count;
     }
 
     @Override
     public long bucketSize(String key) {
-        return 0;
+        long count = 0;
+        int index = hashFunctionOne(key);
+        Node current = arr[index];
+
+        while(current.next != null){
+            count++;
+            current = current.getNext();
+        }
+        return count;
     }
 }
